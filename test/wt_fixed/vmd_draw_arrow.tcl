9ff6b407acc460cb8fcb8abdf4c8c4afa5bab98d
acchangg12 <acchangg12@tamu.edu>
2022-11-12 09:17:57 -0600

    Test folder and files deleted for minimal version tag. Done by acchangg12.

--- test/wt_fixed/vmd_draw_arrow.tcl
+++
@@ -1,7 +0,0 @@
-proc vmd_draw_arrow {mol start end} {
-    # an arrow is made of a cylinder and a cone
-    set middle [vecadd $start [vecscale 0.4 [vecsub $end $start]]]
-    graphics $mol cylinder $start $middle radius 0.4 filled yes
-    graphics $mol cone $middle $end radius 0.6
-}
-		     
